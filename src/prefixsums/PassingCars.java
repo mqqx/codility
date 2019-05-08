f2d3b20962da2acc8892bb8715bddf5494d75779
mx-pwr <hmmrmx@gmail.com>
2019-05-08 09:59:13 +0200

    initial commit

---
+++ src/prefixsums/PassingCars.java
@@ -0,0 +1,41 @@
+package prefixsums;
+
+import java.util.Arrays;
+
+public class PassingCars {
+
+	private static final int MAX_NUMBER_OF_PAIRS_PASSING = 1000000000;
+
+	public static void main(String[] args) {
+		// create an object of this class to have easy access to the solution method without modifying its definition
+		PassingCars passingCars = new PassingCars();
+
+		int[] carArray = {0, 1, 0, 1, 1};
+
+		int expected = 5;
+
+		System.out.println("Array to test: " + Arrays.toString(carArray));
+		System.out.println("Expected: " + expected);
+		System.out.println("Actual: " + passingCars.solution(carArray));
+	}
+
+	public int solution(int[] passingCars) {
+		int countOfPairs = 0;
+		int numberOfCarsTravelingEast = 0;
+
+		for (int car : passingCars) {
+
+			if (car == 0) {
+				numberOfCarsTravelingEast++;
+			}
+
+			if (numberOfCarsTravelingEast > 0 && car == 1) {
+				countOfPairs += numberOfCarsTravelingEast;
+				if (countOfPairs > MAX_NUMBER_OF_PAIRS_PASSING) {
+					return -1;
+				}
+			}
+		}
+		return countOfPairs;
+	}
+}
