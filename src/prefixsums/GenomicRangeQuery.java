f384e31df1147af04ede9fe3b945a0ab5975af2e
mqqx <hmmrmx@gmail.com>
2019-05-17 22:44:32 +0200

    adapted solution to fit performance by using prefix sums

--- src/prefixsums/GenomicRangeQuery.java
+++ src/prefixsums/GenomicRangeQuery.java
@@ -18,26 +18,47 @@ public class GenomicRangeQuery {
 		System.out.println("Actual: " + Arrays.toString(genomicRangeQuery.solution(dnaSequence, queryFrom, queryTo)));
 	}
 
+	// adapted solution https://stackoverflow.com/questions/19552754/java-codility-training-genomic-range-query
 	public int[] solution(String dnaSequence, int[] queryFrom, int[] queryTo) {
+
+		// https://codility.com/media/train/3-PrefixSums.pdf
+		int[][] genomes = new int[3][dnaSequence.length() + 1];
+
+		short nucleotideA, nucleotideC, nucleotideG;
+		for (int i = 0; i < dnaSequence.length(); i++) {
+			nucleotideA = 0;
+			nucleotideC = 0;
+			nucleotideG = 0;
+
+			if ('A' == (dnaSequence.charAt(i))) {
+				nucleotideA = 1;
+			} else if ('C' == (dnaSequence.charAt(i))) {
+				nucleotideC = 1;
+			} else if ('G' == (dnaSequence.charAt(i))) {
+				nucleotideG = 1;
+			}
+
+			genomes[0][i + 1] = genomes[0][i] + nucleotideA;
+			genomes[1][i + 1] = genomes[1][i] + nucleotideC;
+			genomes[2][i + 1] = genomes[2][i] + nucleotideG;
+		}
 
 		int[] lowestImpactFactors = new int[queryFrom.length];
 
 		for (int i = 0; i < queryFrom.length; i++) {
 
-			String subDnaSequence = dnaSequence.substring(queryFrom[i], queryTo[i] + 1);
+			int queryFromIndex = queryFrom[i];
+			int queryToIndex = queryTo[i] + 1;
 
-			char lowestNucleotide = 'T';
-			for (int j = 0; j < subDnaSequence.length(); j++) {
-				char nucleotide = subDnaSequence.charAt(j);
-
-				if (nucleotide < lowestNucleotide) {
-					lowestNucleotide = nucleotide;
-					if (lowestNucleotide == 'A') {
-						break;
-					}
-				}
+			if (genomes[0][queryToIndex] - genomes[0][queryFromIndex] > 0) {
+				lowestImpactFactors[i] = 1;
+			} else if (genomes[1][queryToIndex] - genomes[1][queryFromIndex] > 0) {
+				lowestImpactFactors[i] = 2;
+			} else if (genomes[2][queryToIndex] - genomes[2][queryFromIndex] > 0) {
+				lowestImpactFactors[i] = 3;
+			} else {
+				lowestImpactFactors[i] = 4;
 			}
-			lowestImpactFactors[i] = lowestNucleotide == 'A' ? 1 : lowestNucleotide == 'C' ? 2 : lowestNucleotide == 'G' ? 3 : 4;
 		}
 		return lowestImpactFactors;
 	}
